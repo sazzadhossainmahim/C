73481a373099ce7f577a218dafce17cf8faee2fc
Sazzad Hossain <44097425+sazzadhossainmahim@users.noreply.github.com>
2020-12-01 19:50:29 +0600

    Add files via upload

---
+++ Conditional Statement/1.c
@@ -0,0 +1,13 @@
+// Write a C program to accept two integers and check whether they are equal or not.
+#include <stdio.h>
+void main()
+{
+    int int1, int2;
+ 
+    printf("Input the values for Number1 and Number2 : ");
+    scanf("%d %d", &int1, &int2);
+    if (int1 == int2)
+        printf("Number1 and Number2 are equal\n");
+    else
+        printf("Number1 and Number2 are not equal\n");
+}
