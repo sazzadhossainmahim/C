1f136408d6973a6fbd8ec61fd8d36bd3cecc09dd
Sazzad Hossain <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-01-01 23:33:50 +0600

    Add files via upload

---
+++ codeforwin/Bitwise_operator/11.c
@@ -0,0 +1,21 @@
+
+// date 01-01-21
+
+
+#include <stdio.h>
+
+int main()
+{
+    int num, flippedNumber;
+
+    /* Input number from user */
+    printf("Enter any number: ");
+    scanf("%d", &num);
+
+    flippedNumber = ~num;
+
+    printf("Original number = %d (in decimal)\n", num);
+    printf("Number after bits are flipped = %d (in decimal)", flippedNumber);
+
+    return 0;
+}
