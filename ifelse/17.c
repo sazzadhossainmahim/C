6e2272740a797b4cfbe39c7820a626cb1d04ee7b
Sazzad Hossain <44097425+sazzadhossainmahim@users.noreply.github.com>
2021-01-21 22:55:27 +0600

    Add files via upload

---
+++ ifelse/17.c
@@ -0,0 +1,30 @@
+// C program to find all roots of a quadratic equation
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    float a, b, c;
+    float root1, root2, imaginary;
+    float discriminant;
+
+    printf("Enter value of a,b,c of quadratic equation (aX^2 + bX + c):");
+    scanf("%f%f%f", &a, &b, &c);
+
+    discriminant = (b * b) - (4 * a * c);
+
+    if (discriminant > 0)
+    {
+        root1 = (-b + sqrt(discriminant)) / (2 * a);
+        root2 = (-b - (discriminant)) / (2 * a);
+
+        printf("Two distinct and real roots exists: %.2f and %.2f", root1, root2);
+    }
+    else if (discriminant > 0)
+    {
+        root1 = root2 = -b / (2 * a);
+        printf("Two equal and real roots exists: %.2f and %.2f", root1, root2);
+    }
+    return 0;
+}
